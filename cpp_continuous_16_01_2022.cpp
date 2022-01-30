1605443363ef5f2b95ab144f8bab9d6bbd375c9f
ekhalturin-ki15 <1tigrin@mail.ru>
2022-01-30 16:49:55 +0700

    6

--- cpp_continuous_16_01_2022.cpp
+++ cpp_continuous_16_01_2022.cpp
@@ -3,121 +3,17 @@
 #include <vector>
 #include <algorithm>
 
+
+//#include 
+
 using namespace std;
 
-int i;
-
-typedef vector<int> VI;
-
-
-//factor(10) ~ 3 * 10^6
-long long factor(int n)
-{
-    long long ret = 1;
-    for (int i = 2; i <= n; ++i) ret *= i;
-    return ret;
-}
-
-
-void Out(const VI& buf, const int& count)
-{
-    cout << count << " | ";
-    for (int i = 0; i < buf.size() - 1; ++i) cout << buf[i] <<", ";
-    cout << buf.back() << "\n";
-}
-
-
-int K;
-
-
-// какой шаг, буффер для вывода, откуда брать значения
-void permutation(int step, VI& buf, const VI& v, 
-    set<int>& s, int& count, const int& pred, bool flag)
-{
-    //Условие завершения рекурсии
-
-    if (step == K)
-    {
-        if (flag)
-        {
-            cout << "Отсортировано ";
-        }
-
-        //Нет вызова функции
-        //buf[0] = 10;
-
-        Out(buf, count);
-        ++count;
-        return;
-    }
-    //Тело рекурсии
-    //Может произойти ещё вызов
-
-    for (auto it : v)
-    {
-        if (s.count(it)) continue;
-
-        s.insert(it);
-        buf[step] = it;
-
-        bool tmp_flag = flag;
-
-        if (pred > it) flag = false;
-        permutation(step + 1, buf, v, s, count, it, flag);
-
-        //buf[step] = 0;
-        s.erase(it);
-        flag = tmp_flag;
-
-    }
-}
-
-
-VI buf;
-VI v;
-int n, k, C;
-
-multiset<int> ms;
-set< multiset<int> > pull;
-
-void permutation(int step, int choose)
-{
-    if (choose == k)
-    {
-
-        if (pull.count(ms))
-            return;
-
-        Out(buf, C++);
-        pull.insert(ms);
-
-        return;
-    }
-
-    if (step == n)
-        return;
-
-
-    //Сама рекурсия
-    permutation(step + 1, choose);
-
-
-    ms.insert(v[step]);
-    buf[choose] = v[step];
-
-    permutation(step + 1, choose + 1);
-    ms.erase(ms.find(v[step]));
-
-
-}
 
 typedef vector<vector<long long>> VVLL;
 
-long long M = 1e9 + 7;
+long long M = 1e17 + 7;
 long long n;
 VVLL A, B, C, A_N;
-
-
 
 VVLL mul(VVLL A, VVLL B, long long M)
 {
@@ -129,7 +25,12 @@ VVLL mul(VVLL A, VVLL B, long long M)
     {
         for (int x = 0; x < A.size(); ++x)
         {
-            ret[y][x] = (ret[y][x] + A[y][x] * B[x][y]) % M;
+            for (int i = 0; i < A[0].size(); ++i)
+            {
+                ret[y][x] = (ret[y][x] + A[y][i] * B[i][x]) % M;
+            }
+
+            //ret[y][x] = (ret[y][x] + A[y][x] * B[x][y]) % M;
         }
     }
 
@@ -138,6 +39,10 @@ VVLL mul(VVLL A, VVLL B, long long M)
 
 VVLL bin_pow(VVLL A, long long n, long long M)
 {
+    //Выход рекурсии
+    if (n == 1) return A;
+
+    //Тело рекурсии
     VVLL C = bin_pow(A, n / 2, M);
     VVLL ret = mul(C, C, M);
     if (n % 2 == 1)
@@ -166,7 +71,7 @@ int main()
     // H = 2 = B[0].size()
 
 
-    A_N = bin_pow(A , n, M);
+    A_N = bin_pow(A , n - 2, M);
 
 
     cout << A_N.back().back();
